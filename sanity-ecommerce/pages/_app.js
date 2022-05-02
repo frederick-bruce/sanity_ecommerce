37f9077cd01a6ab07485d6b3d8a5fd270844539c
Frederick Bruce <frederick.bruce.dev@gmail.com>
2022-05-02 13:33:45 -0400

    product update
    product functionality

--- sanity-ecommerce/pages/_app.js
+++ sanity-ecommerce/pages/_app.js
@@ -1,10 +1,13 @@
 import React from "react"
+import {Layout} from "../components"
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <Layout>
     <Component {...pageProps} />
+    </Layout> 
   )
 }
 
