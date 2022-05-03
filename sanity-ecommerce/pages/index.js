71207d5eea3cf8d697ce51b14d6557066c1b10c8
Frederick Bruce <frederick.bruce.dev@gmail.com>
2022-05-03 11:08:52 -0400

    Success Page
    Success Page
    Bug Fixes

--- sanity-ecommerce/pages/index.js
+++ sanity-ecommerce/pages/index.js
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-import { client } from "../lib/client"
-
-import { Product, FooterBanner, HeroBanner,  } from "../components"
+import { client } from '../lib/client';
+import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = ({ products, bannerData }) => (
   <div>
-    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-      {console.log(bannerData)}
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
-      <h2>Best Selling Products</h2>
-      <p>Speakers of many variations</p>
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
     </div>
 
     <div className="products-container">
-        {products?.map((product) => <Product  key={product._id} product={product} />)}
+      {products?.map((product) => <Product key={product._id} product={product} />)}
     </div>
 
     <FooterBanner footerBanner={bannerData && bannerData[0]} />
@@ -33,4 +31,4 @@ export const getServerSideProps = async () => {
   }
 }
 
-export default Home
+export default Home;
